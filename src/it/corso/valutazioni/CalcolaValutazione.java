94030bdbbb5930eb8549610dbd6bd2f610717d27
Francesca Pompi <francesca.pompi93@gmail.com>
2021-11-24 17:09:20 +0100

    fine esercizio

--- src/it/corso/valutazioni/CalcolaValutazione.java
+++ src/it/corso/valutazioni/CalcolaValutazione.java
@@ -7,7 +7,14 @@ public class CalcolaValutazione {
 	int percentualeAssenze;
 	float mediaVoti;
 	
-	// metodi
+	// costruttore
+	CalcolaValutazione(int idStudente, int percentualeAssenze, float mediaVoti) {
+		this.idStudente = idStudente;
+		this.percentualeAssenze = percentualeAssenze;
+		this.mediaVoti = mediaVoti;
+	}
+	
+	// metodo
 	String valutazione(int percentualeAssenze, float mediaVoti) {
 		if((this.percentualeAssenze <= 50 && this.percentualeAssenze >= 25) && this.mediaVoti > 2) {
 			return "promosso";
--- src/it/corso/valutazioni/Main.java
+++ src/it/corso/valutazioni/Main.java
@@ -1,10 +1,32 @@
 package it.corso.valutazioni;
 
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-
+		
+		CalcolaValutazione[] randomStudenti = new CalcolaValutazione[20]; 
+		
+		Random random = new Random();
+		
+		int idStudente = 0;
+		int percentualeAssenze;
+		float mediaVoti;
+		
+		for(int i = 0; i < randomStudenti.length; i++) {
+			percentualeAssenze = random.nextInt(100) + 0;
+			mediaVoti = random.nextInt(5) + 0;
+			
+			randomStudenti[i] = new CalcolaValutazione(idStudente, percentualeAssenze, mediaVoti);
+			idStudente = i + 1;
+			
+			System.out.println("Lo studente con id numero " + idStudente + " è " + randomStudenti[i].valutazione(percentualeAssenze, mediaVoti));
+			
+			// System.out.println(randomStudenti[i].mediaVoti + " " + randomStudenti[i].percentualeAssenze);
+		}
+		
 	}
 
 }
