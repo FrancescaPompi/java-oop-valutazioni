2582c245e3cf594fc0e25428f6c4b91587aa02fb
Francesca Pompi <francesca.pompi93@gmail.com>
2021-11-24 16:10:31 +0100

    fatto metodo

--- src/it/corso/valutazioni/CalcolaValutazione.java
+++ src/it/corso/valutazioni/CalcolaValutazione.java
@@ -1,5 +1,22 @@
 package it.corso.valutazioni;
 
 public class CalcolaValutazione {
-
+	
+	// attributi
+	int idStudente;
+	int percentualeAssenze;
+	float mediaVoti;
+	
+	// metodi
+	String valutazione(int percentualeAssenze, float mediaVoti) {
+		if((this.percentualeAssenze <= 50 && this.percentualeAssenze >= 25) && this.mediaVoti > 2) {
+			return "promosso";
+		} else if(this.percentualeAssenze < 25 && this.mediaVoti >= 2) {
+			return "promosso";
+		} else {
+			return "bocciato";
+		}
+		
+	}
+	
 }
